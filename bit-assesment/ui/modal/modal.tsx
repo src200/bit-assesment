93f5efcf2f1b916970b45cd6a4a2be3812c3bfa3
Challa Sharath <csharath@outlook.com>
2021-12-07 17:30:14 +0530

    remove useCallback

--- bit-assesment/ui/modal/modal.tsx
+++ bit-assesment/ui/modal/modal.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode, useCallback } from 'react';
+import React, { ReactNode } from 'react';
 import classNames from 'classnames';
 import { Overlay as ModalBackDrop } from '@src200/bit-assesment.ui.overlay';
 import { ModalContext } from './context';
@@ -24,15 +24,13 @@ export type ModalProps = {
 };
 
 export function Modal({ isOpen, onClose, children, className, size = 'medium', closeOnOutsideClick = false, fullscreen = false, showCloseIcon = true, ...props }: ModalProps) {
-  const resolveClasses = useCallback(
-    () =>
-      classNames(styles.modal, `${className}`, {
-        [styles.modalSmall]: size === 'small',
-        [styles.modalMedium]: size === 'medium',
-        [styles.modalLarge]: size === 'large',
-        [styles.modalFullScreen]: fullscreen,
-      }), [className, size, fullscreen],
-  );
+  const resolveClasses = () =>
+    classNames(styles.modal, `${className}`, {
+      [styles.modalSmall]: size === 'small',
+      [styles.modalMedium]: size === 'medium',
+      [styles.modalLarge]: size === 'large',
+      [styles.modalFullScreen]: fullscreen,
+    });
 
   return (
     <ModalContext.Provider value={{ isOpen, showCloseIcon, onClose }}>
